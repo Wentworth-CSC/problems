f017ea0863804f28c6833234810f803e399c808a
David Russell <53410706+davidr4501@users.noreply.github.com>
2022-08-19 11:12:10 +1200

    Update __init__.py
    Took out input validation

--- mario/__init__.py
+++ mario/__init__.py
@@ -6,15 +6,15 @@ def exists():
     check50.exists("mario.py")
     check50.include("1.txt", "2.txt", "8.txt")
 
-@check50.check(exists)
-def test_reject_negative():
-    """rejects a height of -1"""
-    check50.run("python3 mario.py").stdin("-1").reject()
+#@check50.check(exists)
+#def test_reject_negative():
+#    """rejects a height of -1"""
+#    check50.run("python3 mario.py").stdin("-1").reject()
 
-@check50.check(exists)
-def test0():
-    """rejects a height of 0"""
-    check50.run("python3 mario.py").stdin("0").reject()
+#@check50.check(exists)
+#def test0():
+#    """rejects a height of 0"""
+#    check50.run("python3 mario.py").stdin("0").reject()
 
 @check50.check(exists)
 def test1():
@@ -34,21 +34,21 @@ def test23():
     out = check50.run("python3 mario.py").stdin("8").stdout()
     check_pyramid(out, open("8.txt").read())
 
-@check50.check(exists)
-def test24():
-    """rejects a height of 9, and then accepts a height of 2"""
-    (check50.run("python3 mario.py").stdin("9").reject()
-            .stdin("2").stdout(open("2.txt")).exit(0))
+#@check50.check(exists)
+#def test24():
+#    """rejects a height of 9, and then accepts a height of 2"""
+#    (check50.run("python3 mario.py").stdin("9").reject()
+#            .stdin("2").stdout(open("2.txt")).exit(0))
 
-@check50.check(exists)
-def test_reject_foo():
-    """rejects a non-numeric height of "foo" """
-    check50.run("python3 mario.py").stdin("foo").reject()
+#@check50.check(exists)
+#def test_reject_foo():
+#    """rejects a non-numeric height of "foo" """
+#    check50.run("python3 mario.py").stdin("foo").reject()
 
-@check50.check(exists)
-def test_reject_empty():
-    """rejects a non-numeric height of "" """
-    check50.run("python3 mario.py").stdin("").reject()
+#@check50.check(exists)
+#def test_reject_empty():
+#    """rejects a non-numeric height of "" """
+#    check50.run("python3 mario.py").stdin("").reject()
 
 
 def check_pyramid(output, correct):
