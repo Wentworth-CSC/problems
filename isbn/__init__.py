c00eb9d0093df68cc355326fce2bda51817030d8
David Russell <53410706+davidr4501@users.noreply.github.com>
2024-04-12 14:17:33 +1200

    Update __init__.py
    hides isbn from docstrings

--- isbn/__init__.py
+++ isbn/__init__.py
@@ -7,30 +7,30 @@ def exists():
 
 @check50.check(exists)
 def test_valid_isbn_1():
-    """Validates a valid ISBN: 1112223339"""
+    """Validates a valid ISBN: <1>"""
     check50.run("python isbn.py").stdin("1112223339").stdout("YES").exit()
 
 @check50.check(exists)
 def test_valid_isbn_2():
-    """Validates a valid ISBN: 1234554321"""
+    """Validates a valid ISBN: <2>"""
     check50.run("python isbn.py").stdin("1234554321").stdout("YES").exit()
     
 @check50.check(exists)
 def test_valid_isbn_3():
-    """Validates a valid ISBN: 048665088X"""
+    """Validates a valid ISBN: <3>"""
     check50.run("python isbn.py").stdin("048665088X").stdout("YES").exit()
 
 @check50.check(exists)
 def test_invalid_isbn_1():
-    """rejects an invalid ISBN: 1112223334"""
+    """rejects an invalid ISBN: <4>"""
     check50.run("python isbn.py").stdin("111222333").stdout("NO").exit()
 
 @check50.check(exists)
 def test_invalid_isbn_2():
-    """rejects an invalid ISBN: 1112223339X"""
+    """rejects an invalid ISBN: <5>"""
     check50.run("python isbn.py").stdin("1112223339X").stdout("NO").exit()
 
 @check50.check(exists)
 def test_invalid_isbn_3():
-    """rejects an invalid ISBN: 1234512345"""
+    """rejects an invalid ISBN: <6>"""
     check50.run("python isbn.py").stdin("1234512345").stdout("NO").exit()
