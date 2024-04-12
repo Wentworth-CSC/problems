ad64cc1ea202a07e932b758fade982d6451fdc5c
David Russell <53410706+davidr4501@users.noreply.github.com>
2024-04-12 14:15:37 +1200

    Update __init__.py
    obfuscated docstrings to discourage coding to the tests

--- twttr/__init__.py
+++ twttr/__init__.py
@@ -10,7 +10,7 @@ def exists():
 
 @check50.check(exists)
 def test_twitter():
-    """input of Twitter yields output of Twttr"""
+    """input of <word> yields output of <twitterised word>"""
     input = "Twitter"
     output = "Twttr"
     check50.run("python3 twttr.py").stdin(input, prompt=True).stdout(regex(output), output, regex=True).exit()
@@ -18,7 +18,7 @@ def test_twitter():
 
 @check50.check(exists)
 def test_name():
-    """input of \"What's your name?\" yields output of \"Wht's yr nm?\""""
+    """input of \"<question>?\" yields output of \"<shortened question>?\""""
     input = "What's your name?"
     output = "Wht's yr nm?"
     check50.run("python3 twttr.py").stdin(input, prompt=True).stdout(regex(output), output, regex=True).exit()
@@ -26,15 +26,15 @@ def test_name():
 
 @check50.check(exists)
 def test_cs50():
-    """input of CS50 yields output of CS50"""
-    input = "CS50"
-    output = "CS50"
+    """input of <code> yields output of <code>"""
+    input = "WCSC50"
+    output = "WCSC50"
     check50.run("python3 twttr.py").stdin(input, prompt=True).stdout(regex(output), output, regex=True).exit()
 
 
 @check50.check(exists)
 def test_python():
-    """input of PYTHON yields output of PYTHN"""
+    """input of <language> yields output of <Language>"""
     input = "PYTHON"
     output = "PYTHN"
     check50.run("python3 twttr.py").stdin(input, prompt=True).stdout(regex(output), output, regex=True).exit()
