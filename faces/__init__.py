56f60ace7ab330ffb1af415540489a439f49d19f
David Russell <53410706+davidr4501@users.noreply.github.com>
2025-03-03 09:57:09 +1300

    Update __init__.py

--- faces/__init__.py
+++ faces/__init__.py
@@ -16,6 +16,16 @@ def testGoodbye():
     output = check50.run("python3 faces.py").stdin("Goodbye :(", prompt=False).stdout("Goodbye 🙁").exit()
 
 @check50.check(exists)
+def testMeh():
+    """input of \"Meh :|\" yields output of \"Meh 😐\""""
+    output = check50.run("python3 faces.py").stdin("Meh :|", prompt=False).stdout("Meh 😐").exit()
+    
+@check50.check(exists)
 def testMultiple():
     """input of \"Hello :) Goodbye :(\" yields output of \"Hello 🙂 Goodbye 🙁\""""
     output = check50.run("python3 faces.py").stdin("Hello :) Goodbye :(", prompt=False).stdout("Hello 🙂 Goodbye 🙁").exit()
+
+@check50.check(exists)
+def testhidden():
+    """input of hidden case yields correct output """
+    output = check50.run("python3 faces.py").stdin("Hello :) Meh :| Goodbye :(", prompt=False).stdout("Hello 🙂 Meh 😐 Goodbye 🙁").exit()
