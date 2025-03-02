6ab5d77f1e969bebcf9e36b4aab884e672f1b584
David Russell <53410706+davidr4501@users.noreply.github.com>
2025-03-03 12:02:10 +1300

    Update __init__.py
    removed need for prompts

--- bold/__init__.py
+++ bold/__init__.py
@@ -9,19 +9,19 @@ def exists():
 @check50.check(exists)
 def bob():
     """responds to a name Bob"""
-    check50.run("python bold.py").stdin("Bob").stdout("*Bob*").exit()
+    check50.run("python bold.py").stdin("Bob", prompt="flase").stdout("*Bob*").exit()
 
 @check50.check(exists)
 def red():
     """responds to a colour red"""
-    check50.run("python bold.py").stdin("red").stdout("*red*").exit()
+    check50.run("python bold.py").stdin("red", prompt="flase").stdout("*red*").exit()
 
 @check50.check(exists)
 def sentence():
     """responds to a sentence \"Welcome to Wentworth\""""
-    check50.run("python bold.py").stdin("Welcome to Wentworth").stdout("*Welcome*to*Wentworth*").exit()
+    check50.run("python bold.py").stdin("Welcome to Wentworth", prompt="flase").stdout("*Welcome*to*Wentworth*").exit()
 
 @check50.check(exists)
 def hidden():
     """responds to an unknown test"""
-    check50.run("python bold.py").stdin("42 Green Flies! How awful!").stdout("*42*Green*Flies!*How*awful!*").exit()
+    check50.run("python bold.py").stdin("42 Green Flies! How awful!", prompt="flase").stdout("*42*Green*Flies!*How*awful!*").exit()
