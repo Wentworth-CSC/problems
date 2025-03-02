99a948ff012a725b61cec3d74eb83098c0b402a5
David Russell <53410706+davidr4501@users.noreply.github.com>
2025-03-03 11:49:58 +1300

    Update __init__.py
    Fixed typos in checks

--- bold/__init__.py
+++ bold/__init__.py
@@ -9,19 +9,19 @@ def exists():
 @check50.check(exists)
 def emma():
     """responds to a name Bob"""
-    check50.run("python hello.py").stdin("Bob").stdout("*Bob*").exit()
+    check50.run("python bold.py").stdin("Bob").stdout("*Bob*").exit()
 
 @check50.check(exists)
 def rodrigo():
     """responds to a colour red"""
-    check50.run("python hello.py").stdin("red").stdout("*red*").exit()
+    check50.run("python bold.py").stdin("red").stdout("*red*").exit()
 
 @check50.check(exists)
 def rodrigo():
     """responds to a sentence \"Welcome to Wentworth\""""
-    check50.run("python hello.py").stdin("Welcome to Wentworth").stdout("*Welcome*to*Wentworth*").exit()
+    check50.run("python bold.py").stdin("Welcome to Wentworth").stdout("*Welcome*to*Wentworth*").exit()
 
 @check50.check(exists)
 def rodrigo():
     """responds to an unknown test"""
-    check50.run("python hello.py").stdin("42 Green Flies! How awful!").stdout("*42*Green*Flies!*How*awful!*").exit()
+    check50.run("python bold.py").stdin("42 Green Flies! How awful!").stdout("*42*Green*Flies!*How*awful!*").exit()
