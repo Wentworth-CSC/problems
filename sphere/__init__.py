610dd814534848209f70c57f72692bc55e1266fc
David Russell <53410706+davidr4501@users.noreply.github.com>
2025-03-07 12:43:54 +1300

    Create __init__.py

---
+++ sphere/__init__.py
@@ -0,0 +1,22 @@
+import check50
+import re
+
+@check50.check()
+def exists():
+    """sphere.py exists"""
+    check50.exists("sphere.py")
+
+@check50.check(exists)
+def test1():
+    """input of 1 yields output of 4.188790204"""
+    output = check50.run("python3 sphere.py").stdin("1", prompt=False).stdout("4.188790204").exit()
+
+@check50.check(exists)
+def test14():
+    """input of 15 yields output of 14137.1669385"""
+    output = check50.run("python3 sphere.py").stdin("15", prompt=False).stdout("14137.1669385").exit()
+@check50.check(exists)
+
+def test50():
+    """input of a hidden value yieldsthe correct output"""
+    output = check50.run("python3 sphere.py").stdin("25", prompt=False).stdout("65449.8469375").exit()
