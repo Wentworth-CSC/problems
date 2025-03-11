bdb24c94b820a84b2d08b82c8fb328561d1163fc
David Russell <53410706+davidr4501@users.noreply.github.com>
2025-03-12 12:39:30 +1300

    Update __init__.py
    Removed prompt=False to allow for prompted input

--- sphere/__init__.py
+++ sphere/__init__.py
@@ -9,14 +9,14 @@ def exists():
 @check50.check(exists)
 def test1():
     """input of 1 yields output of 4.188790204"""
-    output = check50.run("python3 sphere.py").stdin("1", prompt=False).stdout("4.188790204").exit()
+    output = check50.run("python3 sphere.py").stdin("1").stdout("4.188790204").exit()
 
 @check50.check(exists)
 def test14():
     """input of 15 yields output of 14137.1669385"""
-    output = check50.run("python3 sphere.py").stdin("15", prompt=False).stdout("14137.1669385").exit()
+    output = check50.run("python3 sphere.py").stdin("15").stdout("14137.1669385").exit()
 @check50.check(exists)
 
 def test50():
     """input of a hidden value yieldsthe correct output"""
-    output = check50.run("python3 sphere.py").stdin("25", prompt=False).stdout("65449.8469375").exit()
+    output = check50.run("python3 sphere.py").stdin("25").stdout("65449.8469375").exit()
